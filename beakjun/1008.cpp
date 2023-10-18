13ee4a26d2b05743c87ab2a6b6417274d1568640
wxodi0 <teayang06@gmail.com>
2023-10-19 01:12:26 +0900

    .

---
+++ beakjun/1008.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  long double a, b;
+  long double num = 0;
+  cin >> a >> b;
+
+  num = a / b;
+  cout << num;
+}
---
+++ beakjun/test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "run Code: Ctrl + Alt + N";
+}
