269ce42f690d621a21c79e1ba34ec0c9ce3406b0
wxodi0 <teayang06@gmail.com>
2023-10-25 01:21:37 +0900

    Add 10950

---
+++ beakjun/반복문/10950.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+  int a;
+  int b, c;
+  scanf("%d", &a);
+  for(int i = 0; i < a; i++) {
+    scanf("%d %d", &b, &c);
+    printf("%d\n", b + c);
+  }
+
+  return 0;
+}
