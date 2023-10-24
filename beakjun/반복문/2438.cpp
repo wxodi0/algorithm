4e534c8d83dd26ba426f0f83c6ed785fab223f4e
wxodi0 <teayang06@gmail.com>
2023-10-25 02:02:25 +0900

    Fix 2438

--- beakjun/반복문/2438.cpp
+++ beakjun/반복문/2438.cpp
@@ -4,6 +4,9 @@ int main() {
   scanf("%d", &n);
 
   for(int i = 0; i < n; i++) {
+    for(int j = 1; j < n - i; j++) {
+      printf(" ");
+    }
     for(int j = 0; j <= i; j++) {
       printf("*");
     }
